e1cf5ad95884155c9906dbe5c8480d1eeb1561e5
DHarte18 <oluwademilade.harte@students.plymouth.ac.uk>
2022-10-20 12:14:12 +0100

    Update main.cpp

--- Tasks/Task-112-Terminalnput/main.cpp
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -16,16 +16,16 @@ int main()
     char c1, c2;
     printf("\n\nPress a key\n");
     c1 = getchar();
-    printf("You entered character %c which has the ASCII code %d\n", c1, c1);
+    printf("You entered character %c which has the ASCII code %d\n", c1, c1);                   // £= ASCII of 163, *= ASCII of 42, " "= ASCII of 32, %= ASCII of 37
 
     printf("\n\nPress another key\n");
     c2 = getchar();
     printf("You entered character %c which has the ASCII code %d\n", c2, c2);
 
     //scanf
-    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
-    int delay_ms;
-    int parsed = scanf("%d", &delay_ms);
+    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");       // When delay= 500 LEDs flash once evry 0.5s
+    int delay_ms;                                                                               // When delay= 500a LEDs flash every 0.5s
+    int parsed = scanf("%d", &delay_ms);                                                        // When delay= a500 as soon as a is pressed, none integer detected and code asks to be restarted
     
     printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
 
