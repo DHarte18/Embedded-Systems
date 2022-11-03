41207ef36dc5516e9e6cf7927aab16f5ff1b5e82
DHarte18 <114659770+DHarte18@users.noreply.github.com>
2022-11-03 12:11:23 +0000

    Revert "Update main.cpp"
    This reverts commit f18065063fe17efe87be53856eec16647c4d5994.

--- Tasks/Task-126-DigitalIn/main.cpp
+++ Tasks/Task-126-DigitalIn/main.cpp
@@ -1,13 +1,12 @@
-##include "mbed.h"
+#include "mbed.h"
 
 DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
 DigitalOut redLED(PC_2); //Red Traffic 1
 
 // main() runs in its own thread in the OS
 int main()
 {
-    int (btnA || btnB);
+    int btnA;
     // Turn OFF the red LED
     redLED = 0;
 
@@ -15,9 +14,8 @@ int main()
     
         // Wait for the button to be pressed
         do {
-            ((btnA = ButtonA) || (btnB = ButtonB)); //Read button A
-        } while ((btnA == 0) || (btnB == 0));
-          
+            btnA = ButtonA; //Read button A
+        } while (btnA == 0);
 
 
         //Toggle the red LED
@@ -28,8 +26,8 @@ int main()
 
         // Wait for the button to be released
         do {
-            ((btnA = ButtonA) || (btnB = ButtonB)); //Read button A
-        } while ((btnA == 1) || (btnB == 1));
+            btnA = ButtonA; //Read button A
+        } while (btnA == 1);
 
         //Wait for noise to settle
         wait_us(10000);
